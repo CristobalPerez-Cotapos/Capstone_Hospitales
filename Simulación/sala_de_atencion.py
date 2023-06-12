cc34c1fea31174b203dcd57341aa816f4734d87f
Felipe Contreras <ficontreras1@uc.cl>
2023-06-12 09:05:35 -0400

    arreglo

--- Simulación/sala_de_atencion.py
+++ Simulación/sala_de_atencion.py
@@ -12,6 +12,7 @@ class SalaDeAtencion(ABC):
         self.pacientes_atendidos = [] # lista de pacientes ya atendidos en el orden en que fueron atendidos
         self.costo = costo
         self.capacidad = capacidad
+        self.hospital = hospital
         self.cantidad_de_pacientes_por_grupo_en_atencion = {i: 0 for i in range(1, 9)}
         self.cantidad_de_pacientes_por_grupo_atendidos = {i: 0 for i in range(1, 9)}
         self.total_de_pacientes_en_atencion = 0
@@ -35,11 +36,11 @@ class SalaDeAtencion(ABC):
 
     def simular_jornada(self):
         for paciente in self.pacientes_atendidos:
-            paciente.tiempo_atencion_unidad_actual += 0.5
+            paciente.tiempo_atencion_unidad_actual += 1
 
         for grupo in self.pacientes_en_atencion:
             for paciente in self.pacientes_en_atencion[grupo]:
-                paciente.tiempo_atencion_unidad_actual += 0.5  # Se mide en días
+                paciente.tiempo_atencion_unidad_actual += 1  # Se mide en días
                 if paciente.tiempo_atencion_unidad_actual >= paciente.tiempo_a_esperar:
                     self.pacientes_atendidos.append(paciente)
                     self.pacientes_en_atencion[grupo].remove(paciente)
@@ -64,7 +65,7 @@ class SalaDeAtencion(ABC):
                 try:
                     costos_muertos += ph.VALOR_RIESGO[i.ruta_paciente[0]][i.grupo_diagnostico][i.ruta_paciente[1]][int(i.tiempo_esperado_muerto * 2) + 1] * ps.COSTO_VIDA
                 except KeyError:
-                    print(f"El paciente {i.id} ha muerto en la unidad {self.codigo} por no tener un valor de riesgo asociado")
+                    print("Pereció la redonda")
                     costos_muertos += ps.COSTO_VIDA
 
             #print(f"El paciente {i.id} ha incurrido en un gasto en la unidad {self.codigo} de {self.costo[i.grupo_diagnostico]}")
